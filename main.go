730fe689a32544bb46083121fa99601d320ca0d5
Akshay Vemul <akshay.vemul@gmail.com>
2024-06-04 22:10:21 +0530

    chore: basics primitives

--- main.go
+++ main.go
@@ -5,4 +5,34 @@ import "fmt"
 func main() {
 
 	fmt.Println("Hello World")
+
+	var count = 0
+
+	isOdd := false
+
+	pointer := &count
+
+	fmt.Println(count, isOdd)
+
+	fmt.Println("Pointer of count", pointer)
+	fmt.Println("Pointer value of count", *pointer)
+
+	type centimeter int
+
+	type feet int
+
+	var height1 centimeter = 175
+
+	var height2 feet = 5
+
+	/**
+	the idea here is they are both different
+	so if assign them to each other it will throw error
+
+	e.g. height1 = height2 , correct way height1 = centimeter(height2)
+
+	**/
+
+	fmt.Println(height1, height2)
+
 }
